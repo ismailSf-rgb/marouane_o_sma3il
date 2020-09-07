370371a14680f6b01ca3830c4e31b6dfa891d6d9
ismailSf-rgb <70476970+ismailSf-rgb@users.noreply.github.com>
2020-09-07 03:44:13 +0200

    2 obstacles 1 ninja. avec un obstacle qui bouge

---
+++ playTested.js
@@ -0,0 +1,23 @@
+var robot1 = document.getElementsByClassName("win");
+robot1.addEventListener("animationstart", listener, false);
+robot1.addEventListener("animationstart", listener, false);
+robot1.addEventListener("animationend", listener, false);
+robot1.addEventListener("animationiteration", listener, false);
+
+robot1.className = "slidein";
+
+function listener(event) {
+    var l = document.createElement("p");
+    switch(event.type) {
+      case "animationstart":
+        l.innerHTML = "Début : durée écoulée : " + event.elapsedTime;
+        break;
+      case "animationend":
+        l.innerHTML = "Fin : durée écoulée : " + event.elapsedTime;
+        break;
+      case "animationiteration":
+        l.innerHTML = "Nouvelle boucle démarrée à : " + event.elapsedTime;
+        break;
+    }
+    document.getElementById("output").appendChild(l);
+  }
